68a166c0c6cb148992d660c757daa23ef15668db
aya-khames <ya443858@gmail.com>
2023-07-05 03:41:47 +0300

    flask edits

--- Handwave_Flask/app.py
+++ Handwave_Flask/app.py
@@ -1,31 +1,27 @@
 from flask import Flask, render_template, request, session, logging, url_for, redirect, flash, Response, send_file
-from sqlalchemy import create_engine
-from sqlalchemy.sql import text
-from sqlalchemy.orm import scoped_session, sessionmaker
+# from sqlalchemy import create_engine
+# from sqlalchemy.sql import text
+# from sqlalchemy.orm import scoped_session, sessionmaker
 
 import cv2
 import numpy as np
 
+# from gtts import gTTS
+# from IPython.display import Audio
+
+import os
 import tensorflow as tf
 from object_detection.utils import config_util
-import os
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as viz_utils
-import sys
-
-from gtts import gTTS
-from IPython.display import Audio
-sys.path.append("C:/Users/Aya/Handwave/Tensorflow/models")
-sys.path
 from object_detection.builders import model_builder
 
+# from passlib.hash import sha256_crypt
 
-from passlib.hash import sha256_crypt
-
-engine = create_engine("mysql+pymysql://root:1234567@localhost/register")
+# engine = create_engine("mysql+pymysql://root:1234567@localhost/register")
 
 
-MAIN_PATH = 'C:/Users/Aya/Handwave'
+MAIN_PATH = 'C:/Users/Aya/Desktop/GP/Handwave'
 WORKSPACE_PATH = MAIN_PATH + '/Tensorflow/workspace'
 SCRIPTS_PATH = MAIN_PATH + '/Tensorflow/scripts'
 APIMODEL_PATH = MAIN_PATH + '/Tensorflow/models'
@@ -40,7 +36,7 @@ camera = cv2.VideoCapture(0)
 width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-db = scoped_session(sessionmaker(bind=engine))
+# db = scoped_session(sessionmaker(bind=engine))
 
 app = Flask(__name__)
 
@@ -50,76 +46,69 @@ def index():
 
 @app.route("/home")
 def home():
-    if 'log' in session:
-        return render_template("home.html")
-    else:
-        flash("You need to login first!", "danger")
-        return redirect(url_for('login'))
+    return render_template("home.html")
+    # if 'log' in session:
+    #     return render_template("home.html")
+    # else:
+    #     flash("You need to login first!", "danger")
+    #     return redirect(url_for('login'))
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        name = request.form.get("name")
-        username = request.form.get("username")
-        password = request.form.get("passIn")
-        confirm = request.form.get("passConf")
-        securePass = sha256_crypt.encrypt(str(password))
+# @app.route("/register", methods=["GET", "POST"])
+# def register():
+#     if request.method == "POST":
+#         name = request.form.get("name")
+#         username = request.form.get("username")
+#         password = request.form.get("passIn")
+#         confirm = request.form.get("passConf")
+#         securePass = sha256_crypt.encrypt(str(password))
         
-        usernamedata = db.execute(text("SELECT users.username FROM users WHERE users.username=:username"), {"username":username}).fetchone()
-        if not (usernamedata is None):
-            flash("This username already in use, try another one!", "danger")
-            return render_template("register.html")
-        elif password == confirm:
-            db.execute(text("INSERT INTO users(name, username, password) VALUES(:name, :username, :password)"), {"name" :name, "username" :username, "password" :securePass})
-            db.commit()
-            flash("your account is created successfully, you can login now", "success")
-            return redirect(url_for('login'))
-        else:
-            flash("Confirmation password isn't matched!", "danger")
-            return render_template("register.html")
+#         usernamedata = db.execute(text("SELECT users.username FROM users WHERE users.username=:username"), {"username":username}).fetchone()
+#         if not (usernamedata is None):
+#             flash("This username already in use, try another one!", "danger")
+#             return render_template("register.html")
+#         elif password == confirm:
+#             db.execute(text("INSERT INTO users(name, username, password) VALUES(:name, :username, :password)"), {"name" :name, "username" :username, "password" :securePass})
+#             db.commit()
+#             flash("your account is created successfully, you can login now", "success")
+#             return redirect(url_for('login'))
+#         else:
+#             flash("Confirmation password isn't matched!", "danger")
+#             return render_template("register.html")
 
-    return render_template("register.html")
+#     return render_template("register.html")
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        username = request.form.get("usernameLog")
-        password = request.form.get("passLog")
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
+#     if request.method == "POST":
+#         username = request.form.get("usernameLog")
+#         password = request.form.get("passLog")
 
-        usernamedata = db.execute(text("SELECT users.username FROM users WHERE users.username=:username"), {"username":username}).fetchone()
-        if usernamedata is None:
-            flash("Wrong Username!", "danger")
-            return render_template("login.html")
-        else:
-            passworddata = db.execute(text("SELECT users.password FROM users WHERE users.username=:username"), {"username":username}).fetchone()
-            for passwrd in passworddata:
-                if not (sha256_crypt.verify(password, passwrd)):
-                    flash("Wrong Password!", "danger")
-                    return render_template("login.html")
-                else:
-                    session["log"] = True
-                    return redirect(url_for('home'))
-    else:
-        if 'log' in session:
-            return redirect(url_for('home'))
-        else:
-            return render_template("login.html")
+#         usernamedata = db.execute(text("SELECT users.username FROM users WHERE users.username=:username"), {"username":username}).fetchone()
+#         if usernamedata is None:
+#             flash("Wrong Username!", "danger")
+#             return render_template("login.html")
+#         else:
+#             passworddata = db.execute(text("SELECT users.password FROM users WHERE users.username=:username"), {"username":username}).fetchone()
+#             for passwrd in passworddata:
+#                 if not (sha256_crypt.verify(password, passwrd)):
+#                     flash("Wrong Password!", "danger")
+#                     return render_template("login.html")
+#                 else:
+#                     session["log"] = True
+#                     return redirect(url_for('home'))
+#     else:
+#         if 'log' in session:
+#             return redirect(url_for('home'))
+#         else:
+#             return render_template("login.html")
 
-@app.route("/logout")
-def logout():
-    session.clear()
-    return redirect(url_for('login'))
+# @app.route("/logout")
+# def logout():
+#     session.clear()
+#     return redirect(url_for('login'))
 
-@app.route("/home/speak", methods=["GET", "POST"])
-def speak():
-    if request.method == "POST":
-        str = request.form.get("str")
-        tts = gTTS(str)
-        tts.save('1.wav')
-        sound_file = '1.wav'
-        Audio(sound_file, autoplay=True)
-        
+
 def generate_frames():
     camera = cv2.VideoCapture(0)
     width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -130,7 +119,7 @@ def generate_frames():
 
     # Restore checkpoint
     ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
-    ckpt.restore(os.path.join(CHECKPOINT_PATH, 'ckpt-21')).expect_partial()
+    ckpt.restore(os.path.join(CHECKPOINT_PATH, 'ckpt-51')).expect_partial()
 
     @tf.function
     def detect_fn(image):
@@ -166,7 +155,7 @@ def generate_frames():
                     category_index,
                     use_normalized_coordinates=True,
                     max_boxes_to_draw=2,
-                    min_score_thresh=.95,
+                    min_score_thresh=.7,
                     agnostic_mode=False)
         
         ret, buffer = cv2.imencode('.jpg', cv2.resize(image_np_with_detections, (800, 600)))
